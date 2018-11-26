1977db24f1fe0e11432bb5a6e2dca6abb5534260
Michał Trojnara <Michal.Trojnara@stunnel.org>
2018-11-26 08:22:57 +0100

    Fixed PKCS11_MODULE_PATH name

--- misc/softhsm-example-token/gen-token.sh
+++ misc/softhsm-example-token/gen-token.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export MODULE_PATH=/usr/lib/libsofthsm2.so
+export PKCS11_MODULE_PATH=/usr/lib/libsofthsm2.so
 
 cat >config.py <<EOF
 DEBUG = True
 SECRET = "secret1"
-PKCS11MODULE = "$MODULE_PATH"
+PKCS11MODULE = "$PKCS11_MODULE_PATH"
 PKCS11PIN = "secret1"
 EOF
 
@@ -14,10 +14,10 @@ softhsm2-util --delete-token --token osslsigncode
 softhsm2-util --init-token --free --label osslsigncode --pin secret1 --so-pin secret2
 
 # create and print a key pair
-pkcs11-tool --module $MODULE_PATH -l -k --key-type rsa:2048 --id a1b2 --label test --pin secret1
-pkcs11-tool --module $MODULE_PATH -l --pin secret1 -O
+pkcs11-tool --module $PKCS11_MODULE_PATH -l -k --key-type rsa:2048 --id a1b2 --label test --pin secret1
+pkcs11-tool --module $PKCS11_MODULE_PATH -l --pin secret1 -O
 
 # create and print a certificate
 openssl req -new -x509 -subj "/CN=TEST" -engine pkcs11 -keyform engine -key "pkcs11:token=osslsigncode;object=test;pin-value=secret1" -out test.crt
 openssl x509 -inform PEM -outform DER -in test.crt -out test.der
-pkcs11-tool --module $MODULE_PATH -l --id a1b2 --label test -y cert -w test.der --pin secret1
+pkcs11-tool --module $PKCS11_MODULE_PATH -l --id a1b2 --label test -y cert -w test.der --pin secret1
