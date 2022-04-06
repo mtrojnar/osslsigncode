d177cdec2d908cc18b90c05e732b731d70f1022e
olszomal <Malgorzata.Olszowka@stunnel.org>
2022-04-06 12:13:40 +0200

    disable the confusing error created while searching for the certificate

--- osslsigncode.c
+++ osslsigncode.c
@@ -4645,6 +4645,7 @@ static STACK_OF(X509) *PEM_read_certs_with_pass(BIO *bin, char *certpass)
 		sk_X509_push(certs, x509);
 		x509 = PEM_read_bio_X509(bin, NULL, NULL, certpass);
 	}
+	ERR_clear_error();
 	if (!sk_X509_num(certs)) {
 		sk_X509_free(certs);
 		return NULL;
@@ -4659,7 +4660,6 @@ static STACK_OF(X509) *PEM_read_certs(BIO *bin, char *certpass)
 		certs = PEM_read_certs_with_pass(bin, NULL);
 	return certs;
 }
-
 
 static off_t get_file_size(const char *infile)
 {
@@ -5241,6 +5241,7 @@ static int read_crypto_params(GLOBAL_OPTIONS *options, CRYPTO_PARAMS *cparams)
 	/* PKCS11 engine and module support */
 	} else if ((options->p11engine) || (options->p11module)) {
 		ENGINE *engine;
+
 		if (options->p11engine)
 			engine = dynamic_engine(options);
 		else
