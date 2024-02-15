b2024cee9d5125d789930670fe993309c9677f6a
olszomal <Malgorzata.Olszowka@stunnel.org>
2024-02-15 13:40:29 +0100

    Add -ignore-cdp option to help

--- osslsigncode.c
+++ osslsigncode.c
@@ -3039,6 +3039,7 @@ static void usage(const char *argv0, const char *cmd)
         printf("%12s[ -TSA-CRLfile <infile> ]\n", "");
         printf("%12s[ -index <index> ]\n", "");
         printf("%12s[ -ignore-timestamp ]\n", "");
+        printf("%12s[ -ignore-cdp ]\n", "");
         printf("%12s[ -time <unix-time> ]\n", "");
         printf("%12s[ -require-leaf-hash {md5,sha1,sha2(56),sha384,sha512}:XXXXXXXXXXXX... ]\n", "");
         printf("%12s[ -verbose ]\n\n", "");
