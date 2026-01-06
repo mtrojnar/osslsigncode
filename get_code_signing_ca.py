6631a5f10b2c2dfde39a332e59dab4dfaa4a8e62
Michał Trojnara <Michal.Trojnara@stunnel.org>
2026-01-06 14:51:26 +0100

    Update trusted certificate list

--- get_code_signing_ca.py
+++ get_code_signing_ca.py
@@ -10,6 +10,7 @@ from csv import reader
 from requests import get
 from requests.exceptions import RequestException
 from concurrent.futures import ThreadPoolExecutor
+from re import search
 
 def download_cert(hash):
     for attempt in range(10):
@@ -17,23 +18,33 @@ def download_cert(hash):
             sleep(10)
         try:
             creds = f'{attempt}{hash}:{attempt}{hash}'
-            resp = get(f'https://crt.sh/?d={hash}',
-                proxies=dict(https=f'socks5://{creds}@127.0.0.1:9050'))
+            proxies = dict(https=f'socks5://{creds}@127.0.0.1:9050')
+
+            url = f'https://crt.sh/?sha1={hash}&match=='
+            resp = get(url, proxies=proxies)
             resp.raise_for_status()
+
+            m = search(r'\bid=(\d+)\b', resp.content.decode('ascii', 'replace'))
+            id = m.group(1)
+
+            url = f'https://crt.sh/?d={id}'
+            resp = get(url, proxies=proxies)
+            resp.raise_for_status()
+
             print('.', file=stderr, end='')
             stderr.flush()
-            return resp.content.decode('utf-8')
-        except RequestException as e:
-            print(f'\nAttempt {attempt}: {e}', file=stderr)
+            return resp.content.decode('utf-8', 'replace')
+        except Exception as e:
+            print(f'\n{url} attempt {attempt}: {e}', file=stderr)
     print('\nGiving up on', hash, file=stderr)
 
-resp = get('https://ccadb-public.secure.force.com/microsoft/IncludedCACertificateReportForMSFTCSV')
+resp = get('https://ccadb.my.salesforce-sites.com/microsoft/IncludedCACertificateReportForMSFTCSV')
 resp.raise_for_status()
 lines = resp.content.decode('utf-8').splitlines()[1:]
 hashes = [row[4] for row in reader(lines)
     if row[0] != 'Disabled'
         or row[4] == 'F38406E540D7A9D90CB4A9479299640FFB6DF9E224ECC7A01C0D9558D8DAD77D']
-with ThreadPoolExecutor(max_workers=20) as executor:
+with ThreadPoolExecutor(max_workers=10) as executor:
     certs = executor.map(download_cert, hashes)
 for cert in certs:
     if cert is not None:
